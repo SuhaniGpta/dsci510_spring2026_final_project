0c8f72eafcf82efd40186870d7d1507bc4da88d9
Suhani <suhanigu@usc.edu>
2026-04-06 17:24:36 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,9 +2,13 @@ from data_collection import get_trends_data
 
 def test_trends_data():
     data = get_trends_data()
+
     assert data is not None
     assert not data.empty
     assert "Resident Evil Requiem" in data.columns
+
+    print("--- Data Sample ---")
+    print(data.head())
 
 if __name__ == "__main__":
     test_trends_data()
